740a7ff724277a93ea4effe3c69974581c818233
Adán Diaz <adamdiaz@pursuit.org>
2019-11-22 11:18:16 -0500

    set up first view controller, however im missing the background color

--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/MainViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/MainViewController.swift
@@ -21,9 +21,11 @@ class MainViewController: UIViewController {
     
   override func viewDidLoad() {
     super.viewDidLoad()
+    tableView.dataSource = self
     loadData()
     
   }
+    
 
     // prepare(for: ) goes here
     
@@ -33,3 +35,25 @@ class MainViewController: UIViewController {
 
 }
 
+extension MainViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayola.count
+      }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell" , for: indexPath)
+        
+        let crayon = crayola[indexPath.row]
+        
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = crayon.hex
+//        cell.backgroundColor? = figure out a way to change the color of each individual row
+        
+        return cell
+    }
+    
+  
+}
